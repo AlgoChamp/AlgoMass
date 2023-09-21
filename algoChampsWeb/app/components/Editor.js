2ee7e311219f794c1c31f63d7abb5b137bb49721
ajkraus04 <ajkraus04@gmail.com>
2023-09-20 21:56:40 -0400

    can send test info back to front end

--- algoChampsWeb/app/components/Editor.js
+++ algoChampsWeb/app/components/Editor.js
@@ -12,10 +12,10 @@ const IDE = () => {
 
   const [code, setCode] = useState(initialCodeSnippet);
 
-  const handleRunButtonClick = () => {
+  const handleRunButtonClick = async () => {
     console.log('button clicked')
     console.log(code);
-    fetch('http://localhost:3000/submit', {
+    const data = await fetch('http://localhost:3000/submit', {
       method: 'POST',
       body: JSON.stringify({
         src: code,
@@ -25,9 +25,29 @@ const IDE = () => {
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
-      },
+      }
     });
+    const redisData = await data.json()
+    let redisState ='';
+    let redisComp;
+    console.log(redisData)
+    const fibonacci = (n) => {
+      if (n<=1)return n;
+      return fibonacci(n-2) + fibonacci(n-1)
+    }
+    fibonacci(44)
+     const redisReq = await fetch(redisData.data, {
+      headers: {'Accept': 'application/json'}
+    })
+    redisComp =  await redisReq.json()
+    redisState = redisData.status
+    // console.log(await data.json())
+  
+
+    console.log(redisComp)
+   
   };
+
 
   return (
     <div className='flex flex-col items-center justify-center w-1/2 h-3/4'>
