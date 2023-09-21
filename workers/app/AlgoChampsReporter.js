d03c540ed3af0e28b482d9acc85ef3f8b79525fd
ajaxabernathy <ajax.abernathy@gmail.com>
2023-09-21 15:01:16 -0400

    finished pass/fail for presentation

--- workers/app/AlgoChampsReporter.js
+++ workers/app/AlgoChampsReporter.js
@@ -1,35 +1,37 @@
-class CustomReporter{
-    constructor(globalConfig, reporterOptions, reporterContext){
-
+class CustomReporter {
+  constructor(globalConfig, reporterOptions, reporterContext) {
     this.testResults = [];
-        this._globalConfig = globalConfig;
-        this._options = reporterOptions;
-        this._context = reporterContext;
-        this.results = [];
-    }
-    
-    onTestCaseResult(test, testResult, aggregatedResult){
-        /*
+    this._globalConfig = globalConfig;
+    this._options = reporterOptions;
+    this._context = reporterContext;
+    this.results = [];
+  }
+
+  onTestCaseResult(test, testResult, aggregatedResult) {
+    /*
         Properties needed
         testResult
         */
-       const regex = /Received:(.*)/gmi
+    const regex = /Received:(.*)/gim;
 
-       const results = testResult.title.split(' ')
-       if (testResult.state === 'passed') this.results.push([testResult.status.trim(), results[1], results[1]])
-       else {
-        
-        // console.log("The mess", testResult.failureMessages[0])
-        const match = testResult.failureMessages[0].match(regex)
-        this.results.push([testResult.status.trim(), results[1], match[0].replace('Received:','')])
+    const results = testResult.title.split(' ');
+    // console.log(testResult.status);
+    if (testResult.status === 'passed')
+      this.results.push([testResult.status.trim(), results[1], results[1]]);
+    else {
+      // console.log("The mess", testResult.failureMessages[0])
+      const match = testResult.failureMessages[0].match(regex);
+      this.results.push([
+        testResult.status.trim(),
+        results[1],
+        match[0].replace('Received:', ''),
+      ]);
     }
-    }
+  }
 
-    onRunComplete(testContext, aggregatedResults) {
-        console.log(this.results)
-    }
-
-
+  onRunComplete(testContext, aggregatedResults) {
+    console.log(this.results);
+  }
 }
 
 module.exports = CustomReporter;
